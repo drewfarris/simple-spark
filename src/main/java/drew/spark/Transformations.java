86f072323d4d506592fe85a5c972373cbdcdc300
Drew Farris <drew.farris@gmail.com>
2020-10-28 21:37:24 -0400

    Initial commit

---
+++ src/main/java/drew/spark/Transformations.java
@@ -0,0 +1,12 @@
+package drew.spark;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+
+public class Transformations {
+
+    public long myCounter(Dataset<Row> df){
+        return df.count();
+    }
+
+}
